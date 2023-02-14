780a1fc38d36d30c1ef1814bd2ce04bbf16cae1d
Николай <ibraevnikolay@mail.ru>
2023-02-14 16:15:45 +0500

    Создали 9 папку находили макс цифру двухзначног числа

---
+++ 9/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+int num = new Random().Next(10, 100);
+int a1 = num / 10; // находим первую цифру из двухзначного числа
+int a2 = num % 10; // находим остаток двухзначного числа, т.е вторую цифру
+int max = a1;
+if(max < a2)
+  max = a2;
+Console.WriteLine($"Максимальная цифра числа {num} -> {max}");
